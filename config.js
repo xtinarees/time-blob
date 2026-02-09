1cbf9f481d6beae7a141edc4b5a184ab5e24d02e
Christina Rees <reesxtina@gmail.com>
2026-02-08 20:24:29 -0600

    remove config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Time Blob Configuration
-// This file is excluded from production deployment.
-// Change TEST_MODE to true to enable test controls.
-
-window.TIME_BLOB_CONFIG = {
-  TEST_MODE: true,
-};
