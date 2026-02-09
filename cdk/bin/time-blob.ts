840ec683cc07592928b56ca621dbeb706dfd9768
Christina Rees <reesxtina@gmail.com>
2026-02-08 22:52:34 -0600

    fix cdk deployment

--- cdk/bin/time-blob.ts
+++ cdk/bin/time-blob.ts
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CertificateStack } from '../lib/certificate-stack';
-import { TimeBlobStack } from '../lib/time-blob-stack';
+import * as cdk from "aws-cdk-lib";
+import { CertificateStack } from "../lib/certificate-stack";
+import { TimeBlobStack } from "../lib/time-blob-stack";
 
 const app = new cdk.App();
 
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 
 // ACM certificate must be in us-east-1 for CloudFront
-const certStack = new CertificateStack(app, 'TimeBlobCertStack', {
-  env: { account, region: 'us-east-1' },
+const certStack = new CertificateStack(app, "TimeBlobCertStack", {
+  env: { account, region: "us-east-1" },
   crossRegionReferences: true,
 });
 
 // Main stack in us-east-2
-new TimeBlobStack(app, 'TimeBlobStack', {
-  env: { account, region: 'us-east-2' },
+new TimeBlobStack(app, "TimeBlobStack", {
+  env: { account, region: "us-east-2" },
   crossRegionReferences: true,
   certificate: certStack.certificate,
   hostedZone: certStack.hostedZone,
